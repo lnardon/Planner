82f8a1361928b38ff48c81fff5b205029d61c7b6
lnardon <lucaslnardon@gmail.com>
2024-01-24 03:10:34 -0300

    ✨ Add FE task creation and status update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,90 @@
 import "./App.css";
 import { Calendar } from "@/components/ui/calendar";
-import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState<any[]>([]);
   const [date, setDate] = useState<Date | undefined>(new Date());
 
+  async function handleCreateTask() {
+    const task = prompt("Enter task name");
+    if (task) {
+      const res = await fetch("/createTask", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          task,
+          date,
+        }),
+      });
+
+      if (res.ok) {
+        alert("Task created");
+      } else {
+        alert("Error creating task");
+      }
+    }
+  }
+
+  async function handleUpdateTask(taskId: string) {
+    const res = await fetch("/updateTask", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        taskId,
+        status: "completed",
+      }),
+    });
+
+    if (res.ok) {
+      alert("Task updated");
+    } else {
+      alert("Error updating task");
+    }
+  }
+
+  useEffect(() => {
+    fetch("/getTasks", {}).then((res) => {
+      if (res.ok) {
+        res.json().then((data) => {
+          setTasks(data || []);
+        });
+      }
+    });
+  }, []);
+
   return (
-    <>
-      <h1 className="text-4xl font-bold underline">Planner</h1>
-      <Calendar
-        mode="single"
-        selected={date}
-        onSelect={setDate}
-        className="rounded-md border"
-      />
-    </>
+    <div className="app">
+      <div className="sidebar">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          className="rounded-md border"
+        />
+        <Button onClick={handleCreateTask}>Add task</Button>
+      </div>
+      <div className="content">
+        <h1 className="text-4xl font-bold underline">{date?.toDateString()}</h1>
+        <div>
+          {tasks.map((task) => (
+            <div key={task.id} className="flex items-center">
+              <input
+                type="checkbox"
+                checked={task.status === "completed"}
+                onChange={() => handleUpdateTask(task.id)}
+              />
+              <p className="ml-2">{task.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
 
