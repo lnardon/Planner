20d5df82e2a333da3bbd95f8d0cd68dc6d152b02
lnardon <lucaslnardon@gmail.com>
2024-01-23 22:56:15 -0300

    ✨ Add shadecn calendar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import { Calendar } from "@/components/ui/calendar";
+import { useState } from "react";
 
 function App() {
+  const [date, setDate] = useState<Date | undefined>(new Date());
+
   return (
     <>
-      <h1>Daily Planner</h1>
+      <h1 className="text-4xl font-bold underline">Planner</h1>
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border"
+      />
     </>
   );
 }
