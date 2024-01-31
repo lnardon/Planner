d3858ceb8a8b8208bf340443e4f64cb1e5ea3eae
lnardon <lucaslnardon@gmail.com>
2024-01-31 00:55:36 -0300

    💄 Small ui changes

--- frontend/src/components/Timesheet/index.tsx
+++ frontend/src/components/Timesheet/index.tsx
@@ -264,40 +264,45 @@ const Timesheet = ({
       </DialogContent>
 
       <Drawer open={isDrawerOpen} onOpenChange={setIsDrawerOpen}>
-        <DrawerContent className="w-full flex items-center bg-green-500">
-          <div className="w-96 mx-4 my-8 p-4 bg-black bg-opacity-80 rounded-md">
-            <DrawerHeader>
-              <DrawerTitle className="mb-2">{drawerEvent?.name}</DrawerTitle>
-              {drawerEvent?.description && (
-                <DrawerDescription className="mb-2">
-                  {drawerEvent?.description}
-                </DrawerDescription>
-              )}
-              {drawerEvent && (
-                <div className="flex gap-0.5">
-                  <Badge className="text-sm font-bold bg-black text-white border-solid border-2 border-white">{`${
-                    drawerEvent?.start % 12
-                  }:00 ${drawerEvent?.start < 12 ? "am" : "pm"}`}</Badge>
-                  <ChevronRight />
-                  <Badge className="text-sm font-bold bg-black text-white border-solid border-2 border-white">{`${
-                    drawerEvent.end % 12
-                  }:59 ${drawerEvent?.end < 12 ? "am" : "pm"}`}</Badge>
-                </div>
-              )}
-            </DrawerHeader>
-            <DrawerFooter>
-              <Button
-                className="bg-red-600 font-bold text-white"
-                onClick={handleDeleteEvent}
-              >
-                Delete
-              </Button>
-              <DrawerClose>
-                <Button variant="outline" className="w-full">
-                  Cancel
+        <DrawerContent className="w-full flex items-center">
+          <div className="min-w-96 max-w-[35rem] w-full mx-2 my-4 p-6 bg-green-500 rounded-md">
+            <div className="p-2 bg-black bg-opacity-80 rounded-md">
+              <DrawerHeader>
+                <DrawerTitle className="mb-0 text-2xl">
+                  {drawerEvent?.name}
+                </DrawerTitle>
+                <Separator className="mb-2 bg-white" />
+                {drawerEvent?.description && (
+                  <DrawerDescription className="mb-4">
+                    {drawerEvent?.description}
+                  </DrawerDescription>
+                )}
+                {drawerEvent && (
+                  <div className="flex gap-0.5">
+                    <Badge className="text-sm font-bold bg-black text-white border-solid border-2 border-white hover:bg-white hover:text-black">{`${
+                      drawerEvent?.start % 12
+                    }:00 ${drawerEvent?.start < 12 ? "am" : "pm"}`}</Badge>
+                    <ChevronRight />
+                    <Badge className="text-sm font-bold bg-black text-white border-solid border-2 border-white hover:bg-white hover:text-black">{`${
+                      drawerEvent.end % 12
+                    }:59 ${drawerEvent?.end < 12 ? "am" : "pm"}`}</Badge>
+                  </div>
+                )}
+              </DrawerHeader>
+              <DrawerFooter>
+                <DrawerClose>
+                  <Button variant="outline" className="w-full hover:bg-black">
+                    Close
+                  </Button>
+                </DrawerClose>
+                <Button
+                  className="bg-red-600 font-bold text-white hover:bg-red-600"
+                  onClick={handleDeleteEvent}
+                >
+                  Delete
                 </Button>
-              </DrawerClose>
-            </DrawerFooter>
+              </DrawerFooter>
+            </div>
           </div>
         </DrawerContent>
       </Drawer>
