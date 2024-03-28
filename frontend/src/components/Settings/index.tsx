f63eac46fe5ff436204b89931a44e0725cbc8cf3
lnardon <lucaslnardon@gmail.com>
2024-03-28 04:25:41 -0300

    ✨ Add timesheet range settings

--- frontend/src/components/Settings/index.tsx
+++ frontend/src/components/Settings/index.tsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-} from "../ui/sheet";
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from "../ui/sheet";
+import { Button } from "../ui/button";
+import { apiHandler } from "@/utils/apiHandler";
+import { toast } from "react-toastify";
 
 interface Props {
   isSettingsOpen: boolean;
@@ -12,15 +17,84 @@ interface Props {
 }
 
 const Settings: React.FC<Props> = ({ isSettingsOpen, setIsSettingsOpen }) => {
+  const hours = Array.from(
+    { length: 24 },
+    (_, i) => `${i % 12}:00 ${i < 12 ? "am" : "pm"}`
+  );
+  const [startHour, setStartHour] = useState<number | null>(0);
+  const [endHour, setEndHour] = useState<number | null>(23);
+
+  async function handleSave() {
+    let raw = await apiHandler(
+      "/setSettings",
+      "POST",
+      "application/json",
+      JSON.stringify({ startHour, endHour })
+    );
+    if (!raw.ok) {
+      toast.error("Failed to save settings");
+      return;
+    }
+
+    toast.success("Settings saved successfully");
+    setIsSettingsOpen(false);
+  }
+
   return (
     <Sheet open={isSettingsOpen} onOpenChange={setIsSettingsOpen}>
       <SheetContent>
-        <SheetHeader>
-          <SheetTitle className="text-3xl">Settings</SheetTitle>
-          <SheetDescription>
-            Currently there are no settings to configure.
-          </SheetDescription>
+        <SheetHeader className="mb-8">
+          <SheetTitle className="text-4xl">Settings</SheetTitle>
         </SheetHeader>
+        <h4 className="text-sm font-medium text-gray-400 mb-1">
+          Select below the time range to be displayed in the calendar.
+        </h4>
+        <div className="flex items-center gap-4 py-4 mb-4">
+          <Select
+            value={startHour?.toString()}
+            onValueChange={(val) => {
+              setStartHour(parseInt(val));
+            }}
+          >
+            <SelectTrigger className="w-full text-md outline-none border-2 border-gray-400">
+              <SelectValue placeholder="Starts at" />
+            </SelectTrigger>
+            <SelectContent>
+              {hours.map((hour, index) => (
+                <SelectItem
+                  value={index.toString()}
+                  onClick={() => setStartHour(index)}
+                >
+                  {hour}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <span className="text-gray-300 font-medium">to</span>
+          <Select
+            value={endHour?.toString()}
+            onValueChange={(val) => {
+              setEndHour(parseInt(val));
+            }}
+          >
+            <SelectTrigger className="w-full text-md outline-none border-2 border-gray-400">
+              <SelectValue placeholder="Starts at" />
+            </SelectTrigger>
+            <SelectContent>
+              {hours.map((hour, index) => (
+                <SelectItem
+                  value={index.toString()}
+                  onClick={() => setEndHour(index)}
+                >
+                  {hour}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+        <Button className="w-full" onClick={handleSave}>
+          Save
+        </Button>
       </SheetContent>
     </Sheet>
   );
