4247997fb385c1b9446053062b5346247e8f8b4e
lnardon <lucaslnardon@gmail.com>
2024-06-02 22:59:27 -0300

    ♻️ Update docker

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func main () {
     http.Handle("/", http.FileServer(http.Dir("./frontend/dist")))
+    
     http.HandleFunc("/login", handleLogin)
     http.HandleFunc("/register", handleRegister)
     http.HandleFunc("/hasUserRegistered", handleHasUserRegistered)
